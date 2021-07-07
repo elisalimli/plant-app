19e2a91f16aee91adfbc4441c6d4810ecf24840e
alisalim17 <alisalimli1899@gmail.com>
2021-07-07 06:10:06 +0400

    init commit

---
+++ src/navigation/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator;
+
+const Index = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+export default Index;
